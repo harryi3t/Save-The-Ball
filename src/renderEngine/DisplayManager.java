c584a1e11015983bf5d1707c1383613aa0adf6a8
Harendra Singh <2011195@iiitdmj.ac.in>
2015-04-06 14:28:27 +0530

    Code Clean-up

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -14,24 +14,10 @@ public class DisplayManager {
 	}
 	
 	public static void createDisplay(int width, int height){
-		
-		/*ContextAttribs attribs = new ContextAttribs(3, 2)
-			.withForwardCompatible(true)
-			.withProfileCore(true);
-		
-		try {
-			Display.setDisplayMode(new DisplayMode(width, height));
-			Display.create(new PixelFormat(),attribs);
-			Display.setTitle("Save The Ball");
-		} catch (LWJGLException e) {
-			e.printStackTrace();
-		}
-		
-		GL11.glViewport(0, 0, width, height);*/
-		
 		try {
             Display.setDisplayMode(new DisplayMode(width,height));
             Display.create();
+            Display.setTitle("Save The Ball");
             Display.setVSyncEnabled(true);
         } catch (LWJGLException e) {
             e.printStackTrace();
